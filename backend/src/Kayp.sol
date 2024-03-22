3e054623e5b69ec447cea151a41571ba92b0a374
ddiieeggoo b <zkpgmi@gmail.com>
2024-03-22 13:37:34 +0100

    save

--- backend/src/Kayp.sol
+++ backend/src/Kayp.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-contract Kayp {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Kayp is ERC721 {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
