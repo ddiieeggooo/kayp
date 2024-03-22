9a1a88bab3b2cf3858510e28853ca87907d0a05c
ddiieeggoo b <zkpgmi@gmail.com>
2024-03-22 14:27:18 +0100

    save

--- backend/src/Kayp.sol
+++ backend/src/Kayp.sol
@@ -4,9 +4,35 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Kayp is ERC721 {
-    uint256 public number;
 
-    function setNumber(uint256 newNumber) public {
+
+    struct BillOfLading {
+        uint billOfLadingID;
+        uint tripID;
+        string consignor;
+        string oceanVessel;
+        string portOfLoading;
+        string portOfDischarge;
+        uint HScode;
+        uint numberOfPackages;
+        string kindOfPackages;
+        string descriptionOfGoods;
+        uint grossWeight;
+        uint measurement;
+        uint containerNumber;
+        bool isCoveredByInsurance;
+        string placeOfIssue;
+        uint dateOfIssue;
+        uint freightAmount;
+
+
+
+    }
+
+    mapping
+
+
+    function mint(uint256 newNumber) public {
         number = newNumber;
     }
 
