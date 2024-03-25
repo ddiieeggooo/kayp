6f8bb09721f0d5e5bba55599d088e02746a067e0
ddiieeggoo b <zkpgmi@gmail.com>
2024-03-25 20:12:56 +0100

    save

--- backend/src/Kayp.sol
+++ backend/src/Kayp.sol
@@ -2,22 +2,24 @@
 pragma solidity ^0.8.24;
 
 import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
-import {Strings} from "../../utils/Strings.sol";
+import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 /// @title Kayp is a smart contract to create a NFT from a Bill of Lading
 /// @dev The contract is based on the ERC721 standard provided by OpenZeppelin
 /// @author ddiieeggoo
 
-contract Kayp is ERC721 {
+contract Kayp is ERC721, Ownable {
 
-    event HashOfDataCreated(bytes32 hashedBillOfLading);
-    using Strings for uint256;
+    constructor() ERC721("KAYPtoken", "KAYP") Ownable(msg.sender) { }
 
     uint public nftId;
     uint public BillOfLadingID;
     uint public tripID;
     uint public dateOfIssue;
     bytes32 public hashedBillOfLading;
+
+    mapping(uint => bytes32) private nftHashes;
+    mapping(uint => BillOfLading) private billOfLadings;
 
 /// @notice Structure of a Bill of Lading with all the necessary informations
     struct BillOfLading {
@@ -40,37 +42,31 @@ contract Kayp is ERC721 {
         uint freightAmount;
     }
 
-/// @notice this function will retrieve inputs from the user and create a new hash of a Bill of Lading
-/// @param essential data to create a new Bill of Lading
-    function editNewBLWithHash(string calldata _consignor, string calldata _oceabVessel, string calldata _portOfLoading, string calldata _portOfDischarge, uint calldata _HScode, uint calldata _numberOfPackages, string calldata kindOfPackages, string calldata _descriptionOfGoods, uint calldata _grossWeight, uint calldata _measurement, uint calldata _containerCount, bool calldata _isCoveredByInsurance, string calldata _placeOfIssue, uint calldata _dateOfIssue, uint calldata freightAmount) internal returns(bytes32) {
+/// @notice this function will create a new hash of a Bill of Lading and mint a NFT with it
+    function mintNewBLWithHash(string calldata _consignor, string calldata _oceanVessel, string calldata _portOfLoading, string calldata _portOfDischarge, uint _HScode, uint _numberOfPackages, string calldata _kindOfPackages, string calldata _descriptionOfGoods, uint _grossWeight, uint _measurement, uint _containerCount, bool _isCoveredByInsurance, string calldata _placeOfIssue, uint _dateOfIssue, uint _freightAmount) external {
       BillOfLading memory billoflading;
       billoflading.billOfLadingID ++;
       billoflading.tripID ++;
       billoflading.consignor = _consignor;
       billoflading.oceanVessel = _oceanVessel;
       billoflading.portOfLoading = _portOfLoading;
-      billiading.portOfDischarge = _portOfDischarge;
+      billoflading.portOfDischarge = _portOfDischarge;
       billoflading.HScode = _HScode;
       billoflading.numberOfPackages = _numberOfPackages;
       billoflading.kindOfPackages = _kindOfPackages;
       billoflading.descriptionOfGoods = _descriptionOfGoods;
       billoflading.grossWeight = _grossWeight;
       billoflading.measurement = _measurement;
-      billoflading.containerNumber = _containerCount;
+      billoflading.containerCount = _containerCount;
       billoflading.isCoveredByInsurance = _isCoveredByInsurance;
       billoflading.placeOfIssue = _placeOfIssue;
       billoflading.dateOfIssue = _dateOfIssue;
       billoflading.freightAmount = _freightAmount;
       hashedBillOfLading = keccak256(abi.encode(billoflading));
-      emit HashOfDataCreated(hashedBillOfLading);
-      return hashedBillOfLading;
+      nftHashes[nftId] = hashedBillOfLading;
+      _safeMint(msg.sender, nftId);
     }
 
-/// @notice this function inherit _safeMint from ERC721 to create a new NFT with data
-    function mint() external {
-      nftId ++;
-      _safeMint(msg.sender, nftId, hashedBillOfLading);
-    }
 
 /// @notice this function inherit _transfer from ERC721 to transfer a NFT from an address to another
 /// @param from the address of the sender, to the address of the receiver, tokenId the id of the NFT
@@ -79,13 +75,13 @@ contract Kayp is ERC721 {
     }
 
 /// @notice this function will retrieve the Bill of Lading datas from the hash bounded to a given NFT
-    function retrieveBLFromNFT (uint _nftId) private returns(string memory) {
-      bytes memory hashedBillOfLading = data[_nftId];
-      return abi.decode(hashedBillOfLading, (string));
-    }
+    // function retrieveBLFromNFT (uint _nftId) external returns(string memory) {
+    //   bytes32 _hashedBillOfLading = nftHashes[_nftId];
+    //   return abi.decode(_hashedBillOfLading, (string));
+    // }
 
 /// @notice this function is a security measure to avoid locking funds in the smart contract
-    function withdraw (uint amount, address receiver) external onlyOwner {
+    function withdraw (uint _amount, address receiver) external onlyOwner {
       (bool received, ) = msg.sender.call{value: _amount}("");
       require(received, "An error occured");
     }
@@ -93,5 +89,7 @@ contract Kayp is ERC721 {
 /// @notice this function is a fallback in case of funds received on the smart contract
     fallback() external payable {
     }
+
+    receive() external payable {}
 
 }
--- backend/test/Kayp.t.sol
+++ backend/test/Kayp.t.sol
@@ -1,53 +1,64 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+// // SPDX-License-Identifier: MIT
+// pragma solidity 0.8.24;
 
-import "forge-std/Test.sol";
-import "../src/Kayp.sol";
-import "forge-std/console.sol";
+// import "forge-std/Test.sol";
+// import "../src/Kayp.sol";
+// import "forge-std/console.sol";
 
-contract KaypTest is Test {
+// contract KaypTest is Test {
 
-    address owner = makeAddr('User0');
-    address addr1 = makeAddr('User1');
-    address addr2 = makeAddr('User2');
+//     address owner = makeAddr('User0');
+//     address addr1 = makeAddr('User1');
+//     address addr2 = makeAddr('User2');
 
-    Kayp public _Koting;
+//     Kayp public _Kayp;
 
-    struct BillOfLading {
-        uint billOfLadingID;
-        uint tripID;
-        string consignor;
-        string oceanVessel;
-        string portOfLoading;
-        string portOfDischarge;
-        uint HScode;
-        uint numberOfPackages;
-        string kindOfPackages;
-        string descriptionOfGoods;
-        uint grossWeight;
-        uint measurement;
-        uint containerNumber;
-        bool isCoveredByInsurance;
-        string placeOfIssue;
-        uint dateOfIssue;
-        uint freightAmount;
-    }
-
-    function setUp() public {
-        vm.prank(owner);
-        _Kayp = new Kayp();
-    }
+//     event HashOfDataCreated(bytes32 hashedBillOfLading);
 
 
-    function test_expectEmit_HashOfDataCreated() public {
-        BillOfLading memory billoflading;
-        billoflading.billOfLadingID ++;
-        billoflading.tripID ++;
-        hashedBillOfLading = keccak256(abi.encode(billoflading));
-        emit _Kayp.HashOfDataCreated(billoflading);
-        _Kayp.editNewBLWithHash("consignorTest", "oceanVesselTest", "portOfLoadingTest", "portOfDischargeTest", 1234, 10, "kindOfPackagesTest", "descriptionOfGoodsTest", 100, 100, 10, true, "placeOfIssueTest", 24032024, 1000);
-        }
-}
+//     uint public nftId;
+//     uint public BillOfLadingID;
+//     uint public tripID;
+//     uint public dateOfIssue;
+//     bytes32 public hashedBillOfLading;
+
+//     mapping(uint => bytes32) private nftHashes;
+
+//     struct BillOfLading {
+//         uint billOfLadingID;
+//         uint tripID;
+//         string consignor;
+//         string oceanVessel;
+//         string portOfLoading;
+//         string portOfDischarge;
+//         uint HScode;
+//         uint numberOfPackages;
+//         string kindOfPackages;
+//         string descriptionOfGoods;
+//         uint grossWeight;
+//         uint measurement;
+//         uint containerCount;
+//         bool isCoveredByInsurance;
+//         string placeOfIssue;
+//         uint dateOfIssue;
+//         uint freightAmount;
+//     }
+
+//     function setUp() public {
+//         vm.prank(owner);
+//         _Kayp = new Kayp();
+//     }
 
 
-      //  assertEq(hashedBillOfLading, keccak256(abi.encode(billoflading)));
+//     function test_expectEmit_HashOfDataCreated() public {
+//         BillOfLading memory billoflading;
+//         billoflading.billOfLadingID ++;
+//         billoflading.tripID ++;
+//         hashedBillOfLading = keccak256(abi.encode(billoflading));
+//         emit _Kayp.HashOfDataCreated(billoflading);
+//         _Kayp.mintNewBLWithHash("consignorTest", "oceanVesselTest", "portOfLoadingTest", "portOfDischargeTest", 1234, 10, "kindOfPackagesTest", "descriptionOfGoodsTest", 100, 100, 10, true, "placeOfIssueTest", 24032024, 1000);
+//         }
+// }
+
+
+//       //  assertEq(hashedBillOfLading, keccak256(abi.encode(billoflading)));
