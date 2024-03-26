24c053f89cc9c4720f04248d6cf1b5e94fad7ba3
ddiieeggoo b <zkpgmi@gmail.com>
2024-03-26 17:59:27 +0100

    smart contrat updated

--- backend/src/Kayp.sol
+++ backend/src/Kayp.sol
@@ -3,76 +3,73 @@ pragma solidity ^0.8.24;
 
 import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "lib/openzeppelin-contracts/contracts/utils/Strings.sol";
 
-/// @title Kayp is a smart contract to create a NFT from a Bill of Lading
+/// @title Kayp is a smart contract to create a NFT bounded to a Bill of Lading, wich data are stored in the smart contract
 /// @dev The contract is based on the ERC721 standard provided by OpenZeppelin
 /// @author ddiieeggoo
 
-contract Kayp is ERC721, Ownable {
+contract Kayp is ERC721, Ownable{
 
-    constructor() ERC721("KAYPtoken", "KAYP") Ownable(msg.sender) { }
+  constructor() ERC721("KAYPtoken", "KAYP") Ownable(msg.sender) { }
 
-    uint public nftId;
-    uint public BillOfLadingID;
-    uint public tripID;
+  struct StructDeBL {
+      string _BLandNFTid;
+      string tripID;
+      string consignor;
+      string oceanVessel;
+      string portOfLoading;
+      string portOfDischarge;
+      string HScode;
+      string numberOfPackages;
+      string kindOfPackages;
+      string descriptionOfGoods;
+      string grossWeight;
+      string measurement;
+      string containerCount;
+      string placeOfIssue;
+      string dateOfIssue;
+      string freightAmount;
+  }
 
-    mapping (uint => uint) public nftIDtoBillOfLadingID;
 
-    BillOfLadingUints[9] arrayOfBLUints;
-    BillOfLadingStrings[7] arrayOfBLStrings;
+  uint256 public BLandNFTid;
 
-/// @notice Structure of uints of a Bill of Lading
-    struct BillOfLadingUints {
-        uint billOfLadingID;
-        uint tripID;
-        uint HScode;
-        uint numberOfPackages;
-        uint grossWeight;
-        uint measurement;
-        uint containerCount;
-        uint dateOfIssue;
-        uint freightAmount;
-    }
+  StructDeBL public instanceDeBL;
 
-/// @notice Structure of strings of a Bill of Lading
-    struct BillOfLadingStrings {
-        string consignor;
-        string oceanVessel;
-        string portOfLoading;
-        string portOfDischarge;
-        string kindOfPackages;
-        string descriptionOfGoods;
-        string placeOfIssue;
-    }
+  StructDeBL[] public arrayDeTousLesBL;
 
-/// @notice this function will create a new hash of a Bill of Lading and mint a NFT with it
-    function mintNewBLToken(uint[9] calldata _arrayOfBLUints, string[7] calldata _arrayOfBLStrings) external {
-      BillOfLadingUints memory billofladinguints;
-      BillOfLadingStrings memory billofladingstrings;
-      billofladinguints = BillOfLadingUints(_arrayOfBLUints[0], _arrayOfBLUints[1], _arrayOfBLUints[2], _arrayOfBLUints[3], _arrayOfBLUints[4], _arrayOfBLUints[5], _arrayOfBLUints[6], _arrayOfBLUints[7], _arrayOfBLUints[8]);
-      billofladingstrings = BillOfLadingStrings(_arrayOfBLStrings[0], _arrayOfBLStrings[1], _arrayOfBLStrings[2], _arrayOfBLStrings[3], _arrayOfBLStrings[4], _arrayOfBLStrings[5], _arrayOfBLStrings[6]);
-      nftId ++;
-      nftIDtoBillOfLadingID[nftId] = billofladinguints.billOfLadingID;
-      _safeMint(msg.sender, nftId);
-    }
+  mapping (uint BLandNFTid => StructDeBL) public linkBetweenIdandDataOfBL;
 
-/// @notice this function inherit _transfer from ERC721 to transfer a NFT from an address to another
-/// @param from the address of the sender, to the address of the receiver, tokenId the id of the NFT
-    function transferNft(address from, address to, uint256 tokenId) external {
-      _transfer(from, to, tokenId);
-    }
 
-/// @notice this function is a security measure to avoid locking funds in the smart contract
-    function withdraw (uint _amount, address receiver) external onlyOwner {
-      (bool received, ) = receiver.call{value: _amount}("");
-      require(received, "An error occured");
-    }
+  function mintBLToken(string[] memory _arrayDeDatasDuFront) external {
 
-/// @notice this function is a fallback in case of funds received on the smart contract
-    fallback() external payable {
-    }
+    require(_arrayDeDatasDuFront.length >= 16, "Input array must contain at least 16 elements.");
+    uint256 _BLandNFTid = arrayDeTousLesBL.length;
 
-/// @notice this function is a receive in case of funds received on the smart contract
-    receive() external payable {}
+    StructDeBL memory _instanceDeBL = StructDeBL({
+      _BLandNFTid: Strings.toString(_BLandNFTid),
+      tripID: _arrayDeDatasDuFront[1],
+      consignor: _arrayDeDatasDuFront[2],
+      oceanVessel: _arrayDeDatasDuFront[3],
+      portOfLoading: _arrayDeDatasDuFront[4],
+      portOfDischarge: _arrayDeDatasDuFront[5],
+      HScode: _arrayDeDatasDuFront[6],
+      numberOfPackages: _arrayDeDatasDuFront[7],
+      kindOfPackages: _arrayDeDatasDuFront[8],
+      descriptionOfGoods: _arrayDeDatasDuFront[9],
+      grossWeight: _arrayDeDatasDuFront[10],
+      measurement: _arrayDeDatasDuFront[11],
+      containerCount: _arrayDeDatasDuFront[12],
+      placeOfIssue: _arrayDeDatasDuFront[13],
+      dateOfIssue: _arrayDeDatasDuFront[14],
+      freightAmount: _arrayDeDatasDuFront[15]
+    });
+
+    arrayDeTousLesBL.push(_instanceDeBL);
+
+    linkBetweenIdandDataOfBL[_BLandNFTid] = _instanceDeBL;
+    _safeMint(msg.sender, _BLandNFTid);
+  }
 
 }
