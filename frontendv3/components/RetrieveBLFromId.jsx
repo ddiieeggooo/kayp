b2b1a4911fd4da83124f32fad1a36a21a155d8e4
ddiieeggoo b <zkpgmi@gmail.com>
2024-03-31 23:11:05 +0200

    save

--- frontendv3/components/RetrieveBLFromId.jsx
+++ frontendv3/components/RetrieveBLFromId.jsx
@@ -1,53 +1,54 @@
-'use client'
+import { publicClient } from '@/utils/client';
+import { contractAddress, contractAbi } from "@/constants/index";
+import { useAccount, useReadContract } from 'wagmi';
 import { useState } from 'react';
-import { useReadContract, useAccount } from 'wagmi';
-import { readContract } from '@wagmi/core'
-import { UseReadContractParameters } from 'wagmi';
-import { contractAbi, contractAddress } from '@/constants';
-import { Box, Button, Input, Text, VStack, useToast } from '@chakra-ui/react';
 
-const { data: tupleOfData} = useReadContract({
-  address: contractAddress,
-  abi: contractAbi,
-  functionName: 'retrieveBLFromId',
-  args: [tokenId],
-  account: address
-})
-
-const RetrieveBLFromId = () => {
+const SearchVoter = () => {
   const { address } = useAccount();
-
   const [tokenId, setTokenId] = useState('');
 
-  return (
-    <VStack spacing={4}>
-      <Input
-        placeholder="Enter your Token ID in order to retrieve your Bill of lading"
-        value={tokenId}
-        onChange={(e) => setTokenId(e.target.value)}
-      />
-      <Button onClick={retrieveBLFromId}>
-        Retrieve Bill of Lading
-      </Button>
-        <Box>
-          <Text>{tupleOfData}</Text>
-          <Text>{result}</Text>
+  const handleReadTupleClick = async () => {
+          const readTuple = await publicClient.readContract({
+              address: contractAddress,
+              abi: contractAbi,
+              functionName: "getVoter",
+              account: address,
+              args: [tokenId]
+          });
+      }
+  };
 
-            {/* <Text>Bill of lading Id : {result[0]}</Text>
-            <Text>Trip id : $TupleOfData[1]</Text>
-            <Text>Ocean Vessel : data[2]</Text>
-            <Text>Port of Loading and consignor = result[3]</Text>
-            <Text>Port of discharge and consignee = result.data[4]</Text>
-            <Text>HS code = {tupleOfData[5]}</Text>
-            <Text>Number and kind of packages : {tupleOfData[6]}</Text>
-            <Text>Description of goods :</Text>
-            <Text>Gross weight and measurement : </Text>
-            <Text>Container count : </Text>
-            <Text>Place and date of issue </Text>
-            <Text>Freight amount : </Text> */}
-        </Box>
-    </VStack>
+  return (
+      <>
+          <Box mb={4}>
+              <Input
+                  placeholder="Enter Bl Id"
+                  value={tokenId}
+                  onChange={(e) => setTokenId(e.target.value)}
+              />
+              <Button
+                  onClick={handleReadTupleClick}
+                  loadingText="Searching..."
+              >
+                  Search Bill of Lading
+              </Button>
+          </Box>
+          <Box>
+              <Text>Bill of lading Id : </Text>
+              <Text>Trip id : </Text>
+              <Text>Ocean Vessel : </Text>
+              <Text>Port of Loading and consignor = </Text>
+              <Text>Port of discharge and consignee = </Text>
+              <Text>HS code = </Text>
+              <Text>Number and kind of packages : </Text>
+              <Text>Description of goods :</Text>
+              <Text>Gross weight and measurement : </Text>
+              <Text>Container count : </Text>
+              <Text>Place and date of issue </Text>
+              <Text>Freight amount : </Text>
+          </Box>
+      </>
   );
 };
 
-export default RetrieveBLFromId;
+export default SearchVoter;
