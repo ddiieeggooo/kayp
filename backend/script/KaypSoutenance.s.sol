b8f2ff0edd21ea8c8fe9f00e2db8ae05e68794ca
ddiieeggoo b <zkpgmi@gmail.com>
2024-04-05 17:43:06 +0200

    scripts ok

--- backend/script/KaypSoutenance.s.sol
+++ backend/script/KaypSoutenance.s.sol
@@ -1,34 +1,19 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.24;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
 
-// import {Script, console} from "forge-std/Script.sol";
-// import { Kayp } from '../src/Kayp.sol';
-// import { DevOpsTools } from "foundry-devops/src/DevOpsTools.sol";
+import {Script, console} from "forge-std/Script.sol";
+import { Kayp } from '../src/Kayp.sol';
+import { DevOpsTools } from "foundry-devops/src/DevOpsTools.sol";
 
-//    function mintBillOfLadingWithThisData() public {
-//       vm.startBroadcast();
-//       address contractAddress = DevOpsTools.get_most_recent_deployment("Kayp", block.chainid);
-//       Kayp kayp = Kayp(contractAddress);
-//       string[12] memory arrayDuScript = ["", "Cyril", "et", "Ben", "ils", "sont", "trop", "gentils", "I", "need", "rust", "courses"];
-//       kayp.mintBLToken(arrayDuScript);
-//       vm.stopBroadcast();
-//     }
-// }
+contract KaypSoutenance is Script {
 
-
-//     cours
-
-
-
-//     function run() external {
-//         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("SimpleStorage", block.chainid);
-//         interactSimpleStorage(mostRecentlyDeployed);
-//     }
-
-//     doc
-
-//     function interactWithPreviouslyDeployedContracts() public {
-//     address contractAddress = DevOpsTools.get_most_recent_deployment("MyContract", block.chainid);
-//     MyContract myContract = MyContract(contractAddress);
-//     myContract.doSomething();
-// }
+   function run() public {
+      vm.startBroadcast();
+      address contractAddress = DevOpsTools.get_most_recent_deployment("Kayp", block.chainid);
+      console.log(contractAddress);
+      Kayp kayp = Kayp(contractAddress);
+      string[12] memory arrayDuScript = ["", "Cyril", "et", "Ben", "ils", "sont", "trop", "gentils", "I", "need", "rust", "courses"];
+      kayp.mintBLToken(arrayDuScript);
+      vm.stopBroadcast();
+    }
+}
